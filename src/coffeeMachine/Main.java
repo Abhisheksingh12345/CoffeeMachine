377b4ae01ea88d3dc1efc0d440dc20375a6551b6
Mokey-D-luffy88 <atching123@gmail.com>
2020-04-09 17:51:33 +0530

    this will use for user input

--- src/coffeeMachine/Main.java
+++ src/coffeeMachine/Main.java
@@ -1,12 +1,22 @@
 package coffeeMachine;
 
+import java.util.Scanner;
+
 public class Main {
     private static int initialWaterSupply = 400;
     private static int initialMilkSupply = 540;
     private static int initialCoffeeBeanSupply = 120;
     private static int initialDisposableCupSupply = 9;
     private static int initialMoneySupply = 550;
+    private static Scanner tell = new Scanner(System.in);
     public static void main(String[] args) {
 	// write your code here
+        menu();
+    }
+    public static void menu(){
+        boolean comeOut = false;
+        while(!comeOut){
+        System.out.println("Write action (buy, fill, take, remaining, exit): ");
+        String choice =
     }
 }
