9d4b8d99aae94a1f60059479b7c51b009968b68f
Mokey-D-luffy88 <atching123@gmail.com>
2020-04-09 17:35:46 +0530

    these are the globle fields which are we going to change and use frequently in program

---
+++ src/coffeeMachine/Main.java
@@ -0,0 +1,12 @@
+package coffeeMachine;
+
+public class Main {
+    private static int initialWaterSupply = 400;
+    private static int initialMilkSupply = 540;
+    private static int initialCoffeeBeanSupply = 120;
+    private static int initialDisposableCupSupply = 9;
+    private static int initialMoneySupply = 550;
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
